b2ed2bc03c069ab2edab16cd3be302718bc68cf7
Crois0509 <crois0509@naver.com>
2024-08-01 14:42:07 +0900

    feat: BodyCondition 타입 추가

---
+++ CodeStarterCamp_Week4/BodyCondition.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+class BodyCondition {
+    var 상체근력: Int
+    var 하체근력: Int
+    var 근지구력: Int
+    var 피로도: Int
+    
+    init(상체근력: Int, 하체근력: Int, 근지구력: Int, 피로도: Int) {
+        self.상체근력 = 상체근력
+        self.하체근력 = 하체근력
+        self.근지구력 = 근지구력
+        self.피로도 = 피로도
+    }
+}
+
