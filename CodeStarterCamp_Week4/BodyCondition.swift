673edcd11b1051b1dd82197eb579fa88c1cb4750
Crois0509 <crois0509@naver.com>
2024-08-01 14:45:59 +0900

    feat: BodyCondition의 변화상태 출력

--- CodeStarterCamp_Week4/BodyCondition.swift
+++ CodeStarterCamp_Week4/BodyCondition.swift
@@ -1,10 +1,26 @@
 import Foundation
 
 class BodyCondition {
-    var 상체근력: Int
-    var 하체근력: Int
-    var 근지구력: Int
-    var 피로도: Int
+    var 상체근력: Int {
+        didSet {
+            print("근력 변화량: \(oldValue) -> \(상체근력)")
+        }
+    }
+    var 하체근력: Int {
+        didSet {
+            print("근력 변화량: \(oldValue) -> \(하체근력)")
+        }
+    }
+    var 근지구력: Int {
+        didSet {
+            print("근력 변화량: \(oldValue) -> \(근지구력)")
+        }
+    }
+    var 피로도: Int {
+        didSet {
+            print("피로도 변화량: \(oldValue) -> \(피로도)")
+        }
+    }
     
     init(상체근력: Int, 하체근력: Int, 근지구력: Int, 피로도: Int) {
         self.상체근력 = 상체근력
