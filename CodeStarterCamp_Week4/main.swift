c1dc3ec40cdc3db598363b0af19638c920b0b1a7
Crois0509 <crois0509@naver.com>
2024-08-01 14:57:01 +0900

    test: startExercise 메소드 작동 테스트

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -1,1 +1,8 @@
 import Foundation
+
+var crois: Person = Person(name: "crois", age: 24)
+
+윗몸일으키기.startExercise(crois)
+스쿼트.startExercise(crois)
+오래달리기.startExercise(crois)
+동적휴식.startExercise(crois)
